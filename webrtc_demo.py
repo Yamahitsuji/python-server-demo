035d2f4c085adfe9f2aabab767641b6fec7e520b
Yamahitsuji <goat06131004@gmail.com>
2021-10-31 09:14:31 +0900

    feat: add webrtc demo

---
+++ webrtc_demo.py
@@ -0,0 +1,145 @@
+import argparse
+import asyncio
+import json
+import os
+import ssl
+
+from aiohttp import web
+from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+from av import VideoFrame
+
+import numpy as np
+
+
+ROOT = os.path.dirname(__file__)
+
+
+class VideoImageTrack(VideoStreamTrack):
+    """
+    A video stream track that returns a rotating image.
+    """
+
+    def __init__(self):
+        super().__init__()  # don't forget this!
+        self.color = 0
+
+    async def recv(self):
+        pts, time_base = await self.next_timestamp()
+
+        img = np.zeros((500, 500, 3), dtype=np.uint8)
+        img[:, :] = (self.color, self.color, self.color)
+        frame = VideoFrame.from_ndarray(img, format="bgr24")
+        frame.pts = pts
+        frame.time_base = time_base
+
+        return frame
+
+
+async def index(request):
+    content = open(os.path.join(ROOT, "webrtc_template/index.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+
+async def javascript(request):
+    content = open(os.path.join(ROOT, "webrtc_template/client.js"), "r").read()
+    return web.Response(content_type="application/javascript", text=content)
+
+
+async def offer(request):
+    params = await request.json()
+    offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
+
+    pc = RTCPeerConnection()
+    pcs.add(pc)
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        print("Connection state is %s" % pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    await pc.setRemoteDescription(offer)
+    pc.addTrack(VideoImageTrack())
+
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return web.Response(
+        content_type="application/json",
+        text=json.dumps(
+            {"sdp": pc.localDescription.sdp, "type": pc.localDescription.type}
+        ),
+    )
+
+
+pcs = set()
+
+
+async def on_shutdown(app):
+    # close peer connections
+    coros = [pc.close() for pc in pcs]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+
+def aiohttp_server():
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_get("/", index)
+    app.router.add_get("/client.js", javascript)
+    app.router.add_post("/offer", offer)
+    runner = web.AppRunner(app)
+    return runner
+
+
+def run_server(runner, args):
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None
+
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    loop.run_until_complete(runner.setup())
+    site = web.TCPSite(runner, args.host, args.port, ssl_context=ssl_context)
+    loop.run_until_complete(site.start())
+    loop.run_forever()
+
+
+def run(args):
+    if args.cert_file:
+        ssl_context = ssl.SSLContext()
+        ssl_context.load_cert_chain(args.cert_file, args.key_file)
+    else:
+        ssl_context = None
+
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_get("/", index)
+    app.router.add_get("/client.js", javascript)
+    app.router.add_post("/offer", offer)
+    web.run_app(
+        app, access_log=None, host=args.host, port=args.port, ssl_context=ssl_context
+    )
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="WebRTC webcam demo")
+    parser.add_argument("--cert-file", help="SSL certificate file (for HTTPS)")
+    parser.add_argument("--key-file", help="SSL key file (for HTTPS)")
+    parser.add_argument(
+        "--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)"
+    )
+    parser.add_argument(
+        "--port", type=int, default=8080, help="Port for HTTP server (default: 8080)"
+    )
+    arg = parser.parse_args()
+    run(arg)
+
+    # when use thread
+    # server_thread = Thread(target=run_server, args=(aiohttp_server(), arg))
+    # server_thread.start()
+
+
